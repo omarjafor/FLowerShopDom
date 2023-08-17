731590d7dab9d05d6262bd61b5d7cea3b81ac0d6
omarjafor <omarjaforchy@gmail.com>
2023-08-17 20:26:18 +0600

    function js

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
+let total = 0;
 function handleCLikBtn(target){
     const selectedItem = document.querySelector('#selected-items');
     const itemName = target.parentNode.childNodes[1].innerText;
     const p = document.createElement('p');
     p.innerText = itemName;
     selectedItem.appendChild(p);
+    const price = parseInt(target.parentNode.childNodes[5].innerText.split(' ')[1]);
+    total = total + price;
+    console.log(total);
 }
