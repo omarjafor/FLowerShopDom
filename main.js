e65e1d65515cea08a7dc4fa098595715d89916ea
omarjafor <omarjaforchy@gmail.com>
2023-08-17 20:30:49 +0600

    total sum

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ function handleCLikBtn(target){
     const selectedItem = document.querySelector('#selected-items');
     const itemName = target.parentNode.childNodes[1].innerText;
     const p = document.createElement('p');
-    p.innerText = itemName;
-    selectedItem.appendChild(p);
     const price = parseInt(target.parentNode.childNodes[5].innerText.split(' ')[1]);
+    p.innerText = itemName + ' = ' + price +' Taka';
+    selectedItem.appendChild(p);
     total = total + price;
-    console.log(total);
+    document.getElementById('total').innerText = total;
 }
